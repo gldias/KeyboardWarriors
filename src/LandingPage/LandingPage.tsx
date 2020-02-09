42e4e46dffe01a564863ea00087bf81e0f795e03
Alan19 <Alan19@users.noreply.github.com>
2020-02-09 13:23:37 -0500

    Make LandingPage conform to project conventions

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -10,16 +10,16 @@ export class LandingPage extends React.Component {
   render() {
     return (
       <Container>
-        <img style={{display: 'block', margin: 'auto'}} src={kbWarriorsLogo} alt={"A black and orange helmet"}/>
-        <h1 style={{textAlign: 'center'}}>Keyboard Warriors</h1>
+        <img className="logo" src={kbWarriorsLogo} alt="A black and orange helmet"/>
+        <h1 className="title">Keyboard Warriors</h1>
         <Row className="justify-content-md-center button-row">
           <Col xs={3} lg={2}>
-            <Button className={"fight-button landing-button"} variant={"primary"}>
+            <Button className="fight-button landing-button" variant="primary">
               Fight!
             </Button>
           </Col>
           <Col xs={{span: 3, offset: 1}} lg={{span: 2, offset: 1}}>
-            <Button className={"leaderboard-button landing-button"} variant={"primary"}>
+            <Button className="leaderboard-button landing-button" variant="primary">
               Leaderboard
             </Button>
           </Col>
