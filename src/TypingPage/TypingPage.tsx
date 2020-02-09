69ecafc1f48de5f186414815d8a4b0bd9705a1b5
Guilherme Dias <gldias97@gmail.com>
2020-02-09 17:42:14 -0500

    Semi completed visuals for typing page

--- src/TypingPage/TypingPage.tsx
+++ src/TypingPage/TypingPage.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Insult, {InsultState} from "../components/Insult";
+import ListGroup from 'react-bootstrap/ListGroup'
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import "./typingPage.css";
 
 export interface TypingProps {
     insults: string[];
@@ -41,14 +45,23 @@ class TypingPage extends React.Component<TypingProps, TypingState> {
         const {currentInsult, typedText} = this.state;
         
         return (
-            <>
-            {this.props.insults.map((insult, index) => {
-                let state = (index < currentInsult ? InsultState.COMPLETE 
-                    : (index === currentInsult ? InsultState.CURRENT : InsultState.UPCOMING));
-                return <Insult key={insult} text={insult} state={state} typedText={typedText} />
-            })}
-            <input onChange={this.textChanged} value={typedText} />
-            </>
+            <Container className="typing-container">
+                <Row className="justify-content-md-center">
+                    <ListGroup variant="flush">
+                            {this.props.insults.map((insult, index) => {
+                                let state = (index < currentInsult ? InsultState.COMPLETE 
+                                    : (index === currentInsult ? InsultState.CURRENT : InsultState.UPCOMING));
+                                return (
+                                    <ListGroup.Item className="insult-item">
+                                        <Insult key={insult} text={insult} state={state} typedText={typedText} />
+                                    </ListGroup.Item>)
+                            })}
+                    </ListGroup>
+                </Row>
+                <Row className="justify-content-md-center input-box">
+                    <input onChange={this.textChanged} value={typedText} />
+                </Row>
+            </Container>
         );
     }
 }
