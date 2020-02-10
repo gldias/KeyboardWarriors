45a542ba85f7844419a5690da64daa42f11c9cf4
Guilherme Dias <gldias97@gmail.com>
2020-02-09 23:22:47 -0500

    Add header to typing page

--- src/TypingPage/TypingPage.tsx
+++ src/TypingPage/TypingPage.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Insult, {InsultState} from "../components/Insult/Insult";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 import Timer from "../components/Timer/Timer";
+
+import logo from "../resources/keyboardWarriorWhite.png";
+
 import "./typingPage.css";
 
 export interface TypingProps {
@@ -55,17 +59,31 @@ class TypingPage extends React.Component<TypingProps, TypingState> {
         const {currentInsult, typedText} = this.state;
         
         return (
-            <Container className="typing-container">
-                <Timer ref={this._timer}/>
-                {this.props.insults.map((insult, index) => {
-                    let state = (index < currentInsult ? InsultState.COMPLETE 
-                        : (index === currentInsult ? InsultState.CURRENT : InsultState.UPCOMING));
-                    return <Insult key={insult} text={insult} state={state} typedText={typedText} />
-                })}
-                <Row className="justify-content-md-center input-box">
-                    <input onChange={this.textChanged} value={typedText} />
-                </Row>
-            </Container>
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand>
+                        <img
+                            alt="Keyboard Warriors"
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Keyboard Warriors
+                    </Navbar.Brand>
+                </Navbar>
+                <Container className="typing-container">
+                    <Timer ref={this._timer}/>
+                    {this.props.insults.map((insult, index) => {
+                        let state = (index < currentInsult ? InsultState.COMPLETE 
+                            : (index === currentInsult ? InsultState.CURRENT : InsultState.UPCOMING));
+                        return <Insult key={insult} text={insult} state={state} typedText={typedText} />
+                    })}
+                    <Row className="justify-content-md-center input-box">
+                        <input onChange={this.textChanged} value={typedText} />
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
