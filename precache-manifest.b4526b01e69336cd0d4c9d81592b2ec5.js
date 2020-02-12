54a907e5f1616ad4fe9eef6bdc6ca6197921c0f4
Guilherme Dias <gldias97@gmail.com>
2020-02-12 13:41:12 -0500

    Updates

---
+++ precache-manifest.b4526b01e69336cd0d4c9d81592b2ec5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da3025f903af2e3fa92b5abc572b317",
+    "url": "/KeyboardWarriors/index.html"
+  },
+  {
+    "revision": "957a82d83fe44081fb93",
+    "url": "/KeyboardWarriors/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d99997cbdd8e0938930c",
+    "url": "/KeyboardWarriors/static/css/main.70be105c.chunk.css"
+  },
+  {
+    "revision": "957a82d83fe44081fb93",
+    "url": "/KeyboardWarriors/static/js/2.5792f0a2.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/KeyboardWarriors/static/js/2.5792f0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99997cbdd8e0938930c",
+    "url": "/KeyboardWarriors/static/js/main.8e14fee3.chunk.js"
+  },
+  {
+    "revision": "8ed51875b999bd8c3147",
+    "url": "/KeyboardWarriors/static/js/runtime-main.dbc262a9.js"
+  },
+  {
+    "revision": "841d9970b69a411579ae7e04cfae16fc",
+    "url": "/KeyboardWarriors/static/media/keyboardWarriorWhite.841d9970.png"
+  }
+]);
