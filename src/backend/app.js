cfb1b59add3c7589ba7c2ac753d8ec081f67320c
Nick Bova <npb7642@rit.edu>
2020-02-11 20:09:09 -0500

    working on API not getting much luck

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var insults = require('../resources/insults.json');
+
+function getJson(req, res, next){
+    res.send(insults);
+}
+
+getJson(res);
+
+//app.get('../resources/insults.json', (req, res) => {
+    //let data = req;
+    //console.log(data);
+    //return res.send(data);
+  //});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
